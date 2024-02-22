6aadeddf1e1688c4cf483010b16b29a479547504
sda782 <saerdna.petersen@gmail.com>
2024-02-22 23:19:34 +0100

    stuff

--- src/routes/compass/+page.svelte
+++ src/routes/compass/+page.svelte
@@ -10,7 +10,7 @@
   }
 </script>
 
-<div class="container h-full mx-auto flex justify-center">
+<div class="container w-10/12 h-full flex justify-center mx-auto">
   <div class="w-full text-center">
     <h1 class="h1 my-10">Compass</h1>
     <input
