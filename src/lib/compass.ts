895d53d826d8935db9b3a3b33fd7c4d13c101b6d
sda782 <59613370+sda782@users.noreply.github.com>
2024-04-23 09:51:31 +0200

    Update compass.ts

--- src/lib/compass.ts
+++ src/lib/compass.ts
@@ -10,14 +10,14 @@ export type search_history_item = {
     country_code: string | null
 }
 
-export const links: Array<site_link> = [
+export const _links: Array<site_link> = [
     {
         country_code: "dk",
         link: "avalise.vercel.app",
         full_link: ""
     }
 ]
-export const _links: Array<site_link> = [
+export const links: Array<site_link> = [
     {
         country_code: "cz",
         link: "euroskateshop.cz",
